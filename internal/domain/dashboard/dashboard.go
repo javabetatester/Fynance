5143c681d246bdb43a274432ccd82aa1089731d7
BK <birisesports@gmail.com>
2025-10-31 16:24:26 -0300

    fix: remove email param

--- internal/domain/dashboard/dashboard.go
+++ internal/domain/dashboard/dashboard.go
@@ -10,12 +10,11 @@ import (
 type Dashboard struct {
 	UserId          uuid.UUID
 	Name            string
-	Email           string
 	TotalBalance    float64
 	MonthReceipt    float64
 	MonthExpense    float64
-	TotalInvestment float64
 	FixedExpenses   float64
+	TotalInvestment float64
 	Goals           []goal.Goal
 	Transactions    []transaction.Transaction
 }
