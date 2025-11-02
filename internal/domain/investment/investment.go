01200ac58eb78bc82106a1673b3ab88d7366a972
BK <birisesports@gmail.com>
2025-11-02 00:03:23 -0300

    feat: investment layer will be validate

--- internal/domain/investment/investment.go
+++ internal/domain/investment/investment.go
@@ -7,13 +7,13 @@ import (
 )
 
 type Investment struct {
-	Id              int
-	UserId          uuid.UUID
-	Type            Types
-	Name            string
-	TotalAmount     float64
-	CurrentAmount   float64
-	ReturnRate      float64
-	ApplicationDate time.Time
-	UpdatedAt       time.Time
+	Id              int       `gorm:"primaryKey;autoIncrement"`
+	UserId          uuid.UUID `gorm:"type:uuid;index;not null"`
+	Type            Types     `gorm:"not null"`
+	Name            string    `gorm:"size:100;not null"`
+	TotalAmount     float64   `gorm:"not null"`
+	CurrentAmount   float64   `gorm:"not null"`
+	ReturnRate      float64   `gorm:"not null"`
+	ApplicationDate time.Time `gorm:"not null"`
+	UpdatedAt       time.Time `gorm:"not null"`
 }
