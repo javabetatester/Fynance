19118cd828a519c86857d60fa1301db4709dd927
BK <birisesports@gmail.com>
2025-11-02 00:04:56 -0300

    refactor: changes in all of entity

--- internal/domain/user/user.go
+++ internal/domain/user/user.go
@@ -1,16 +1,22 @@
 package user
 
 import (
+	"Fynance/internal/domain/goal"
+	"Fynance/internal/domain/investment"
+	"Fynance/internal/domain/transaction"
 	"time"
 
 	"github.com/google/uuid"
 )
 
 	type User struct {
-		Id        uuid.UUID
-		Name      string
-		Email     string
-		Password  string
-		CreatedAt time.Time
-		UpdatedAt time.Time
+		Id           uuid.UUID                `gorm:"primaryKey;type:uuid"`
+		Name         string                   `gorm:"size:100;not null"`
+		Email        string                   `gorm:"size:100;uniqueIndex;not null"`
+		Password     string                   `gorm:"size:255;not null"`
+		CreatedAt    time.Time                `gorm:"not null"`
+		UpdatedAt    time.Time                `gorm:"not null"`
+		Transactions []transaction.Transaction `gorm:"foreignKey:UserId"`
+		Goals        []goal.Goal              `gorm:"foreignKey:UserId"`
+		Investments  []investment.Investment   `gorm:"foreignKey:UserId"`
 	}
