34b5f81a8b0a48fd23f8fdfdaffaf5a5bab15485
BK <birisesports@gmail.com>
2025-11-05 20:53:51 -0300

    feat: add user existence checks and input validations for transaction and category services

--- internal/domain/transaction/service.go
+++ internal/domain/transaction/service.go
@@ -4,6 +4,7 @@ import (
 	"Fynance/internal/domain/user"
 	"Fynance/internal/utils"
 	"errors"
+	"strings"
 	"time"
 
 	"github.com/oklog/ulid/v2"
@@ -17,6 +18,9 @@ type Service struct {
 }
 
 func (s *Service) CreateTransaction(transaction *Transaction) error {
+	if err := s.ensureUserExists(transaction.UserId); err != nil {
+		return err
+	}
 
 	err := s.CategoryValidation(transaction.CategoryId, transaction.UserId)
 	if err != nil {
@@ -33,31 +37,50 @@ func (s *Service) CreateTransaction(transaction *Transaction) error {
 }
 
 func (s *Service) UpdateTransaction(transaction *Transaction) error {
+	if err := s.ensureUserExists(transaction.UserId); err != nil {
+		return err
+	}
+
+	storedTransaction, err := s.GetTransactionByID(transaction.Id, transaction.UserId)
+	if err != nil {
+		return err
+	}
+
 	transaction.UpdatedAt = time.Now()
 
-	err := s.UpdateTransactionValidation(transaction)
+	err = s.UpdateTransactionValidation(transaction)
 	if err != nil {
 		return err
 	}
 
-	err = s.Repository.Update(transaction)
-	if err != nil {
-		return err
+	storedTransaction.CategoryId = transaction.CategoryId
+	storedTransaction.Amount = transaction.Amount
+	storedTransaction.Description = transaction.Description
+	storedTransaction.Type = transaction.Type
+	if !transaction.Date.IsZero() {
+		storedTransaction.Date = transaction.Date
 	}
+	storedTransaction.UpdatedAt = transaction.UpdatedAt
 
-	return nil
+	return s.Repository.Update(storedTransaction)
 }
 
-func (s *Service) DeleteTransaction(transactionID ulid.ULID) error {
-	err := s.TransactionExists(transactionID)
-	if err != nil {
+func (s *Service) DeleteTransaction(transactionID ulid.ULID, userID ulid.ULID) error {
+	if err := s.TransactionExists(transactionID, userID); err != nil {
 		return err
 	}
 	return s.Repository.Delete(transactionID)
 }
 
-func (s *Service) GetTransactionByID(transactionID ulid.ULID) (*Transaction, error) {
-	return s.Repository.GetByID(transactionID)
+func (s *Service) GetTransactionByID(transactionID ulid.ULID, userID ulid.ULID) (*Transaction, error) {
+	transaction, err := s.Repository.GetByID(transactionID)
+	if err != nil {
+		return nil, err
+	}
+	if transaction.UserId != userID {
+		return nil, errors.New("transaction does not belong to user")
+	}
+	return transaction, nil
 }
 
 func (s *Service) GetAllTransactions(userID ulid.ULID) ([]*Transaction, error) {
@@ -76,9 +99,15 @@ func (s *Service) GetTransactionsByCategory(categoryID ulid.ULID, userID ulid.UL
 	return s.Repository.GetByCategory(categoryID, userID)
 }
 
-// CATEGORYS
-
 func (s *Service) CreateCategory(category *Category) error {
+	if err := s.ensureUserExists(category.UserId); err != nil {
+		return err
+	}
+
+	category.Name = strings.TrimSpace(category.Name)
+	if category.Name == "" {
+		return errors.New("name is required")
+	}
 
 	if err := s.CategoryExists(category.Name, category.UserId); err != nil {
 		return err
@@ -90,39 +119,79 @@ func (s *Service) CreateCategory(category *Category) error {
 }
 
 func (s *Service) UpdateCategory(category *Category) error {
-	return s.CategoryRepository.Update(category)
+	if err := s.ensureUserExists(category.UserId); err != nil {
+		return err
+	}
+
+	existingCategory, err := s.CategoryRepository.GetByID(category.Id, category.UserId)
+	if errors.Is(err, gorm.ErrRecordNotFound) {
+		return errors.New("category not found")
+	}
+	if err != nil {
+		return err
+	}
+
+	category.Name = strings.TrimSpace(category.Name)
+	if category.Name == "" {
+		return errors.New("name is required")
+	}
+
+	if !strings.EqualFold(existingCategory.Name, category.Name) {
+		if err := s.CategoryExists(category.Name, category.UserId); err != nil {
+			return err
+		}
+	}
+
+	existingCategory.Name = category.Name
+	existingCategory.Icon = category.Icon
+	existingCategory.UpdatedAt = time.Now()
+
+	return s.CategoryRepository.Update(existingCategory)
 }
 
 func (s *Service) DeleteCategory(categoryID ulid.ULID, userID ulid.ULID) error {
-	_, err := s.GetCategoryByID(categoryID, userID)
-	if err != nil {
+	if err := s.ensureUserExists(userID); err != nil {
+		return err
+	}
+
+	if _, err := s.CategoryRepository.GetByID(categoryID, userID); errors.Is(err, gorm.ErrRecordNotFound) {
+		return errors.New("category not found")
+	} else if err != nil {
 		return err
 	}
 	return s.CategoryRepository.Delete(categoryID, userID)
 }
 
 func (s *Service) GetCategoryByID(categoryID ulid.ULID, userID ulid.ULID) (*Category, error) {
-	_, err := s.UserService.GetByID(userID.String())
-	if err != nil {
-		return nil, errors.New("user not found")
+	if err := s.ensureUserExists(userID); err != nil {
+		return nil, err
 	}
-	_, err = s.CategoryRepository.GetByID(categoryID, userID)
-	if err != nil {
+
+	category, err := s.CategoryRepository.GetByID(categoryID, userID)
+	if errors.Is(err, gorm.ErrRecordNotFound) {
 		return nil, errors.New("category not found")
 	}
-	return nil, err
+	if err != nil {
+		return nil, err
+	}
+
+	return category, nil
 }
 
 func (s *Service) GetAllCategories(userID ulid.ULID) ([]*Category, error) {
-	_, err := s.UserService.GetByID(userID.String())
-	if err != nil {
-		return nil, errors.New("user not found")
+	if err := s.ensureUserExists(userID); err != nil {
+		return nil, err
 	}
 	return s.CategoryRepository.GetAll(userID)
 }
 
 func (s *Service) CategoryExists(categoryName string, userID ulid.ULID) error {
-	_, err := s.CategoryRepository.GetByName(categoryName, userID)
+	trimmedName := strings.TrimSpace(categoryName)
+	if trimmedName == "" {
+		return errors.New("name is required")
+	}
+
+	_, err := s.CategoryRepository.GetByName(trimmedName, userID)
 
 	if errors.Is(err, gorm.ErrRecordNotFound) {
 		return nil
@@ -172,24 +241,31 @@ func (s *Service) UpdateTransactionValidation(transaction *Transaction) error {
 		return errors.New("amount must be greater than 0")
 	}
 
-	_, err := s.GetCategoryByID(transaction.CategoryId, transaction.UserId)
-	if errors.Is(err, gorm.ErrRecordNotFound) {
-		return errors.New("category does not exist")
-	}
-	if err != nil {
+	if _, err := s.GetCategoryByID(transaction.CategoryId, transaction.UserId); err != nil {
 		return err
 	}
 
 	return nil
 }
 
-func (s *Service) TransactionExists(transactionID ulid.ULID) error {
-	_, err := s.GetTransactionByID(transactionID)
+func (s *Service) TransactionExists(transactionID ulid.ULID, userID ulid.ULID) error {
+	_, err := s.GetTransactionByID(transactionID, userID)
 	if errors.Is(err, gorm.ErrRecordNotFound) {
 		return errors.New("transaction does not exist")
 	}
 	if err != nil {
 		return err
+	}
+	return nil
+}
+
+func (s *Service) ensureUserExists(userID ulid.ULID) error {
+	if s.UserService == nil {
+		return errors.New("user service not configured")
+	}
+	_, err := s.UserService.GetByID(userID.String())
+	if err != nil {
+		return errors.New("user not found")
 	}
 	return nil
 }
