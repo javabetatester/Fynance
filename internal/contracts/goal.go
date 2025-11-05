a6c37a977aa5c04fd3423a072d680f6cc21b759e
BK <birisesports@gmail.com>
2025-11-05 20:52:41 -0300

    feat: add GoalResponse and GoalListResponse types to enhance goal-related API responses

--- internal/contracts/goal.go
+++ internal/contracts/goal.go
@@ -1,6 +1,10 @@
 package contracts
 
-import "time"
+import (
+	"time"
+
+	domainGoal "Fynance/internal/domain/goal"
+)
 
 type GoalCreateRequest struct {
 	Name   string     `json:"name" binding:"required"`
@@ -12,4 +16,13 @@ type GoalUpdateRequest struct {
 	Name   string     `json:"name" binding:"required"`
 	Target float64    `json:"target" binding:"required,gt=0"`
 	EndAt  *time.Time `json:"end_at"`
+}
+
+type GoalResponse struct {
+	Goal *domainGoal.Goal `json:"goal"`
+}
+
+type GoalListResponse struct {
+	Goals []*domainGoal.Goal `json:"goals"`
+	Total int                `json:"total"`
 }
