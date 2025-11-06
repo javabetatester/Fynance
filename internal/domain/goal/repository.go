287e2d48470b9da8c013be17b97330372609b776
BK <birisesports@gmail.com>
2025-11-06 20:36:09 -0300

    refactor: update goal repository methods to accept context for improved request handling

--- internal/domain/goal/repository.go
+++ internal/domain/goal/repository.go
@@ -1,14 +1,18 @@
 package goal
 
-import "github.com/oklog/ulid/v2"
+import (
+	"context"
+
+	"github.com/oklog/ulid/v2"
+)
 
 type Repository interface {
-	Create(goal *Goal) error
-	List() ([]*Goal, error)
-	Update(goal *Goal) error
-	UpdateFields(id ulid.ULID, fields map[string]interface{}) error
-	Delete(id ulid.ULID) error
-	GetById(id ulid.ULID) (*Goal, error)
-	GetByUserId(userId ulid.ULID) ([]*Goal, error)
-	CheckGoalBelongsToUser(goalID ulid.ULID, userID ulid.ULID) (bool, error)
+	Create(ctx context.Context, goal *Goal) error
+	List(ctx context.Context) ([]*Goal, error)
+	Update(ctx context.Context, goal *Goal) error
+	UpdateFields(ctx context.Context, id ulid.ULID, fields map[string]interface{}) error
+	Delete(ctx context.Context, id ulid.ULID) error
+	GetById(ctx context.Context, id ulid.ULID) (*Goal, error)
+	GetByUserId(ctx context.Context, userId ulid.ULID) ([]*Goal, error)
+	CheckGoalBelongsToUser(ctx context.Context, goalID ulid.ULID, userID ulid.ULID) (bool, error)
 }
