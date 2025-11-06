05b1246b5b4832ad8358dea899afd6c018739940
BK <birisesports@gmail.com>
2025-11-06 20:36:58 -0300

    refactor: update user service methods to accept context for improved request handling

--- internal/domain/user/service.go
+++ internal/domain/user/service.go
@@ -1,8 +1,9 @@
 package user
 
 import (
+	"context"
+
 	"Fynance/internal/utils"
-	"errors"
 
 	"github.com/oklog/ulid/v2"
 	"golang.org/x/crypto/bcrypt"
@@ -12,7 +13,7 @@ type Service struct {
 	Repository Repository
 }
 
-func (s *Service) Create(user *User) error {
+func (s *Service) Create(ctx context.Context, user *User) error {
 	user.Id = utils.GenerateULID()
 
 	now := utils.SetTimestamps()
@@ -25,31 +26,31 @@ func (s *Service) Create(user *User) error {
 	}
 	user.Password = string(hashedPassword)
 
-	return s.Repository.Create(user)
+	return s.Repository.Create(ctx, user)
 }
 
-func (s *Service) Update(user *User) error {
-	return s.Repository.Update(user)
+func (s *Service) Update(ctx context.Context, user *User) error {
+	return s.Repository.Update(ctx, user)
 }
 
-func (s *Service) Delete(id string) error {
-	return s.Repository.Delete(id)
+func (s *Service) Delete(ctx context.Context, id string) error {
+	return s.Repository.Delete(ctx, id)
 }
 
-func (s *Service) GetByID(id string) (*User, error) {
-	user, err := s.Repository.GetById(id)
+func (s *Service) GetByID(ctx context.Context, id string) (*User, error) {
+	user, err := s.Repository.GetById(ctx, id)
 	if err != nil {
-		return nil, errors.New("user not found")
+		return nil, err
 	}
 	return user, nil
 }
 
-func (s *Service) GetByEmail(email string) (*User, error) {
-	return s.Repository.GetByEmail(email)
+func (s *Service) GetByEmail(ctx context.Context, email string) (*User, error) {
+	return s.Repository.GetByEmail(ctx, email)
 }
 
-func (s *Service) GetPlan(id ulid.ULID) (Plan, error) {
-	plan, err := s.Repository.GetPlan(id)
+func (s *Service) GetPlan(ctx context.Context, id ulid.ULID) (Plan, error) {
+	plan, err := s.Repository.GetPlan(ctx, id)
 	if err != nil {
 		return "", err
 	}
