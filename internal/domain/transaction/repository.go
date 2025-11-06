36b40f1b8c5ec0331e63c567171fc0327e85d748
BK <birisesports@gmail.com>
2025-11-06 20:36:34 -0300

    refactor: update transaction and category repository methods to accept context for improved request handling

--- internal/domain/transaction/repository.go
+++ internal/domain/transaction/repository.go
@@ -1,27 +1,31 @@
 package transaction
 
-import "github.com/oklog/ulid/v2"
+import (
+	"context"
+
+	"github.com/oklog/ulid/v2"
+)
 
 type Repository interface {
-	Create(transaction *Transaction) error
-	Update(transaction *Transaction) error
-	Delete(transactionID ulid.ULID) error
-	GetByID(transactionID ulid.ULID) (*Transaction, error)
-	GetAll(userID ulid.ULID) ([]*Transaction, error)
-	GetByAmount(amount float64) ([]*Transaction, error)
-	GetByName(name string) ([]*Transaction, error)
-	GetByCategory(categoryID ulid.ULID, userID ulid.ULID) ([]*Transaction, error)
-	GetByInvestmentId(investmentID ulid.ULID, userID ulid.ULID) ([]*Transaction, error)
-	GetNumberOfTransactions(userID ulid.ULID) (int64, error)
+	Create(ctx context.Context, transaction *Transaction) error
+	Update(ctx context.Context, transaction *Transaction) error
+	Delete(ctx context.Context, transactionID ulid.ULID) error
+	GetByID(ctx context.Context, transactionID ulid.ULID) (*Transaction, error)
+	GetAll(ctx context.Context, userID ulid.ULID) ([]*Transaction, error)
+	GetByAmount(ctx context.Context, amount float64) ([]*Transaction, error)
+	GetByName(ctx context.Context, name string) ([]*Transaction, error)
+	GetByCategory(ctx context.Context, categoryID ulid.ULID, userID ulid.ULID) ([]*Transaction, error)
+	GetByInvestmentId(ctx context.Context, investmentID ulid.ULID, userID ulid.ULID) ([]*Transaction, error)
+	GetNumberOfTransactions(ctx context.Context, userID ulid.ULID) (int64, error)
 }
 
 type CategoryRepository interface {
-	Create(category *Category) error
-	Update(category *Category) error
-	Delete(categoryID ulid.ULID, userID ulid.ULID) error
-	GetByID(categoryID ulid.ULID, userID ulid.ULID) (*Category, error)
-	GetAll(userID ulid.ULID) ([]*Category, error)
-	GetByUserID(userID ulid.ULID) ([]*Category, error)
-	BelongsToUser(categoryID ulid.ULID, userID ulid.ULID) (bool, error)
-	GetByName(categoryName string, userID ulid.ULID) (*Category, error)
+	Create(ctx context.Context, category *Category) error
+	Update(ctx context.Context, category *Category) error
+	Delete(ctx context.Context, categoryID ulid.ULID, userID ulid.ULID) error
+	GetByID(ctx context.Context, categoryID ulid.ULID, userID ulid.ULID) (*Category, error)
+	GetAll(ctx context.Context, userID ulid.ULID) ([]*Category, error)
+	GetByUserID(ctx context.Context, userID ulid.ULID) ([]*Category, error)
+	BelongsToUser(ctx context.Context, categoryID ulid.ULID, userID ulid.ULID) (bool, error)
+	GetByName(ctx context.Context, categoryName string, userID ulid.ULID) (*Category, error)
 }
