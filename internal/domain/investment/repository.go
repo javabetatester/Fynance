0be8560ef6d4c11c4cfacb3a79ae89e963621704
BK <birisesports@gmail.com>
2025-11-06 20:36:22 -0300

    refactor: update investment repository methods to accept context for improved request handling

--- internal/domain/investment/repository.go
+++ internal/domain/investment/repository.go
@@ -1,16 +1,18 @@
 package investment
 
-import "github.com/oklog/ulid/v2"
+import (
+	"context"
+
+	"github.com/oklog/ulid/v2"
+)
 
 type Repository interface {
-	Create(investment *Investment) error
-	List(userId ulid.ULID) ([]*Investment, error)
-	Update(investment *Investment) error
-	Delete(id ulid.ULID, userId ulid.ULID) error
-	GetInvestmentById(id ulid.ULID, userId ulid.ULID) (*Investment, error)
-	GetByUserId(userId ulid.ULID) ([]*Investment, error)
-	GetTotalBalance(userId ulid.ULID) (float64, error)
-	GetByType(userId ulid.ULID, investmentType Types) ([]*Investment, error)
+	Create(ctx context.Context, investment *Investment) error
+	List(ctx context.Context, userId ulid.ULID) ([]*Investment, error)
+	Update(ctx context.Context, investment *Investment) error
+	Delete(ctx context.Context, id ulid.ULID, userId ulid.ULID) error
+	GetInvestmentById(ctx context.Context, id ulid.ULID, userId ulid.ULID) (*Investment, error)
+	GetByUserId(ctx context.Context, userId ulid.ULID) ([]*Investment, error)
+	GetTotalBalance(ctx context.Context, userId ulid.ULID) (float64, error)
+	GetByType(ctx context.Context, userId ulid.ULID, investmentType Types) ([]*Investment, error)
 }
-
-
