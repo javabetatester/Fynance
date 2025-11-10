6d4eb1a14fc90b38fd4b0a7e7fe2cd6f78766ee4
BK <birisesports@gmail.com>
2025-11-10 20:20:09 -0300

    refactor: improve user repository error handling by wrapping database errors with custom error types

--- internal/infrastructure/user_repository.go
+++ internal/infrastructure/user_repository.go
@@ -2,8 +2,10 @@ package infrastructure
 
 import (
 	"context"
+	"errors"
 
 	"Fynance/internal/domain/user"
+	appErrors "Fynance/internal/errors"
 
 	"github.com/oklog/ulid/v2"
 	"gorm.io/gorm"
@@ -14,40 +16,59 @@ type UserRepository struct {
 }
 
 func (r *UserRepository) Create(ctx context.Context, user *user.User) error {
-	return r.DB.WithContext(ctx).Create(user).Error
+	if err := r.DB.WithContext(ctx).Create(user).Error; err != nil {
+		return appErrors.NewDatabaseError(err)
+	}
+	return nil
 }
 
 func (r *UserRepository) Update(ctx context.Context, user *user.User) error {
-	return r.DB.WithContext(ctx).Save(user).Error
+	if err := r.DB.WithContext(ctx).Save(user).Error; err != nil {
+		return appErrors.NewDatabaseError(err)
+	}
+	return nil
 }
 
 func (r *UserRepository) Delete(ctx context.Context, id string) error {
-	return r.DB.WithContext(ctx).Where("id = ?", id).Delete(&user.User{}).Error
+	result := r.DB.WithContext(ctx).Where("id = ?", id).Delete(&user.User{})
+	if result.Error != nil {
+		return appErrors.NewDatabaseError(result.Error)
+	}
+	if result.RowsAffected == 0 {
+		return appErrors.ErrUserNotFound
+	}
+	return nil
 }
 
 func (r *UserRepository) GetById(ctx context.Context, id string) (*user.User, error) {
-	var user user.User
-	if err := r.DB.WithContext(ctx).Where("id = ?", id).First(&user).Error; err != nil {
-		return nil, err
+	var entity user.User
+	if err := r.DB.WithContext(ctx).Where("id = ?", id).First(&entity).Error; err != nil {
+		if errors.Is(err, gorm.ErrRecordNotFound) {
+			return nil, appErrors.ErrUserNotFound.WithError(err)
+		}
+		return nil, appErrors.NewDatabaseError(err)
 	}
-	return &user, nil
+	return &entity, nil
 }
 
 func (r *UserRepository) GetByEmail(ctx context.Context, email string) (*user.User, error) {
-	var user user.User
-	if err := r.DB.WithContext(ctx).Where("email = ?", email).First(&user).Error; err != nil {
-		return nil, err
+	var entity user.User
+	if err := r.DB.WithContext(ctx).Where("email = ?", email).First(&entity).Error; err != nil {
+		if errors.Is(err, gorm.ErrRecordNotFound) {
+			return nil, appErrors.ErrUserNotFound.WithError(err)
+		}
+		return nil, appErrors.NewDatabaseError(err)
 	}
-	return &user, nil
+	return &entity, nil
 }
 
 func (r *UserRepository) GetPlan(ctx context.Context, id ulid.ULID) (user.Plan, error) {
-	var u user.User
-
-	err := r.DB.WithContext(ctx).Select("plan").Where("id = ?", id.String()).First(&u).Error
-	if err != nil {
-		return "", err
+	var entity user.User
+	if err := r.DB.WithContext(ctx).Select("plan").Where("id = ?", id.String()).First(&entity).Error; err != nil {
+		if errors.Is(err, gorm.ErrRecordNotFound) {
+			return "", appErrors.ErrUserNotFound.WithError(err)
+		}
+		return "", appErrors.NewDatabaseError(err)
 	}
-
-	return u.Plan, nil
+	return entity.Plan, nil
 }
